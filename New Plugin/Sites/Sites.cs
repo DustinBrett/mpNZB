30170c49bbed8dc9688c58d2a7fa5abe3f9811dd
dustinbrett <dustinbrett@a90ab10a-1d43-11de-9909-f37ff7dbbc12>
2009-05-21 21:22:22 +0000

    Working on a redo of entire code, need a place to put it so I can work on it anywhere.

---
+++ New Plugin/Sites/Sites.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+using MediaPortal.Configuration;
+using MediaPortal.GUI.Library;
+
+namespace mpNZB
+{
+  class Sites
+  {
+
+    #region Declarations
+
+    public string SiteName = String.Empty;
+    public string FeedName = String.Empty;
+    public string FeedURI = String.Empty;
+    public string Cookies = String.Empty;
+
+    Functions MP = new Functions();
+
+    #endregion
+
+    #region Initialization
+
+    public Sites(string _Type)
+    {
+      SetSite(_Type);
+    }
+
+    #endregion
+
+    #region Dialogs
+
+    public void SetSite(string _Type)
+    {
+      try
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Config.GetFolder(Config.Dir.Plugins) + @"\Windows\Sites.xml");
+
+        if (xmlDoc.SelectSingleNode("sites/site") != null)
+        {
+          List<GUIListItem> Sites = new List<GUIListItem>();
+
+          foreach (XmlNode nodeItem in xmlDoc.SelectNodes("sites/site"))
+          {
+            switch (_Type)
+            {
+              case "Feeds":
+                if (nodeItem.SelectSingleNode("feeds/feed") != null) Sites.Add(new GUIListItem(nodeItem.Attributes["id"].InnerText));
+                break;
+              case "Groups":
+                if (nodeItem.SelectSingleNode("groups") != null) Sites.Add(new GUIListItem(nodeItem.Attributes["id"].InnerText));
+                break;
+              case "Search":
+                if (nodeItem.SelectSingleNode("search") != null) Sites.Add(new GUIListItem(nodeItem.Attributes["id"].InnerText));
+                break;
+            }
+          }
+
+          if (Sites.Count > 0)
+          {
+            GUIListItem Site = MP.Menu(Sites, "Select Site");
+            if (Site != null)
+            {
+              SiteName = Site.Label;
+            }
+          }
+        }
+        else
+        {
+          GUIPropertyManager.SetProperty("#Status", "Sites missing.");
+        }
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+
+    public void SetFeed()
+    {
+      try
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Config.GetFolder(Config.Dir.Plugins) + @"\Windows\Sites.xml");
+
+        if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/feeds/feed") != null)
+        {
+          List<GUIListItem> Feeds = new List<GUIListItem>();
+
+          foreach (XmlNode nodeItem in xmlDoc.SelectNodes("sites/site[@id='" + SiteName + "']/feeds/feed"))
+          {
+            Feeds.Add(new GUIListItem(nodeItem.Attributes["name"].InnerText, String.Empty, nodeItem.InnerText, false, null));
+          }
+
+          if (Feeds.Count > 0)
+          {
+            GUIListItem Feed = MP.Menu(Feeds, "Select Feed");
+            if (Feed != null)
+            {
+              FeedName = Feed.Label;
+              FeedURI = Feed.Path.Replace("[MAX]", MP.GetValueAsInt("#Sites", "MaxResults", 50).ToString());
+            }
+          }
+        }
+        else
+        {
+          GUIPropertyManager.SetProperty("#Status", "Feeds missing.");
+        }
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+
+    public void SetGroup()
+    {
+      try
+      {
+        XmlDocument xmlSettings = new XmlDocument();
+        xmlSettings.Load(Config.GetFolder(Config.Dir.Config) + @"\mpNZB.xml");
+
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Config.GetFolder(Config.Dir.Plugins) + @"\Windows\Sites.xml");
+
+        if ((xmlSettings.SelectSingleNode("profile/section[@name='#Groups']/entry") != null) && (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/group") != null))
+        {
+          List<GUIListItem> Groups = new List<GUIListItem>();
+
+          foreach (XmlNode nodeItem in xmlSettings.SelectNodes("profile/section[@name='#Groups']/entry"))
+          {
+            Groups.Add(new GUIListItem(nodeItem.Attributes["name"].InnerText));
+          }
+
+          if (Groups.Count > 0)
+          {
+            GUIListItem Group = MP.Menu(Groups, "Select Group");
+            if (Group != null)
+            {
+              FeedName = Group.Label;
+              FeedURI = xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/group").InnerText.Replace("[GROUP]", FeedName).Replace("[MAX]", MP.GetValueAsInt("#Sites", "MaxResults", 50).ToString());
+            }
+          }
+        }
+        else
+        {
+          GUIPropertyManager.SetProperty("#Status", "Groups missing.");
+        }
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+
+    public void SetSearch()
+    {
+      try
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Config.GetFolder(Config.Dir.Plugins) + @"\Windows\Sites.xml");
+
+        if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/search") != null)
+        {
+          FeedName = MP.Keyboard();
+          if (FeedName.Length > 0)
+          {
+            FeedURI = xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/search").InnerText.Replace("[QUERY]", FeedName.Replace(" ", "+")).Replace("[MAX]", MP.GetValueAsInt("#Sites", "MaxResults", 50).ToString());
+          }
+        }
+        else
+        {
+          GUIPropertyManager.SetProperty("#Status", "Search missing.");
+        }
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+    
+    #endregion
+
+    #region Items
+
+    public void AddItem(XmlNode _Node, GUIListControl _List)
+    {
+      try
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(Config.GetFolder(Config.Dir.Plugins) + @"\Windows\Sites.xml");
+
+        #region Title
+
+        string strTitle = String.Empty;
+        if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/title") != null)
+        {
+          strTitle = ((xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/title[@attribute]") != null) ? _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/title").Attributes["element"].InnerText].Attributes[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/title").Attributes["attribute"].InnerText].InnerText : _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/title").Attributes["element"].InnerText].InnerText).Replace("&quot;", "\"").Replace("&amp;", "&").Replace("&apos;", "'").Replace("&lt;", "<").Replace("&gt;", ">");
+        }
+
+        #endregion
+
+        #region Size
+
+        string strSize = String.Empty;
+        double dblSize = 0;
+        if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size") != null)
+        {
+          switch (int.Parse(xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["type"].InnerText))
+          {
+            case 1:
+              if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size/regex") != null)
+              {
+                Match regSize = Regex.Match(((xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size[@attribute]") != null) ? _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["element"].InnerText].Attributes[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["attribute"].InnerText].InnerText : _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["element"].InnerText].InnerText), @"\w{4}:\s(\d+[,|.]\d+)\s(\w{2,4})");
+                if ((regSize.Success) && (regSize.Groups[1].Value.Length > 0) && (regSize.Groups[2].Value.Length > 0))
+                {
+                  dblSize = double.Parse(Regex.Replace(regSize.Groups[1].Value, "[(.|,)]", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator.ToCharArray()[0].ToString()));
+                  switch (regSize.Groups[2].Value.Replace("KiB", "KB").Replace("MiB", "MB").Replace("GiB", "GB").Replace("TiB", "TB"))
+                  {
+                    case "KB":
+                      dblSize = (dblSize * 1024);
+                      break;
+                    case "MB":
+                      dblSize = ((dblSize * 1024) * 1024);
+                      break;
+                    case "GB":
+                      dblSize = (((dblSize * 1024) * 1024) * 1024);
+                      break;
+                    case "TB":
+                      dblSize = ((((dblSize * 1024) * 1024) * 1024) * 1024);
+                      break;
+                  }
+                }
+              }
+              break;
+            case 2:
+              dblSize = ((xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size[@attribute]") != null) ? double.Parse(_Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["element"].InnerText].Attributes[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["attribute"].InnerText].InnerText) : double.Parse(_Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/size").Attributes["element"].InnerText].InnerText));
+              break;
+          }
+
+          if (dblSize == 0) { strSize = String.Empty; }
+          else if (dblSize < 1024) { strSize = dblSize.ToString("N2") + " B"; }
+          else if (dblSize < 1048576) { strSize = (dblSize / 1024).ToString("N2") + " KB"; }
+          else if (dblSize < 1073741824) { strSize = ((dblSize / 1024) / 1024).ToString("N2") + " MB"; }
+          else if (dblSize < 1099511627776) { strSize = (((dblSize / 1024) / 1024) / 1024).ToString("N2") + " GB"; }
+          else if (dblSize < 1125899906842624) { strSize = ((((dblSize / 1024) / 1024) / 1024) / 1024).ToString("N2") + " TB"; }
+        }
+
+        #endregion
+
+        #region URL
+
+        string strURL = String.Empty;
+        if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url") != null)
+        {
+          strURL = ((xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url[@attribute]") != null) ? _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url").Attributes["element"].InnerText].Attributes[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url").Attributes["attribute"].InnerText].InnerText : _Node[xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url").Attributes["element"].InnerText].InnerText);
+          if (xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item/url/regex") != null)
+          {
+            foreach (XmlNode Replacement in xmlDoc.SelectNodes("sites/site[@id='" + SiteName + "']/item/url/replace"))
+            {
+              strURL = strURL.Replace(Replacement.Attributes["old"].InnerText, Replacement.Attributes["new"].InnerText);
+            }
+          }
+        }
+
+        #endregion
+
+        #region Description
+
+        string strDescription = String.Empty;
+        if (_Node["description"] != null)
+        {
+          strDescription = _Node["description"].InnerText;
+          if (Regex.IsMatch(strDescription, @"<[^>]*>"))
+          {
+            strDescription = Regex.Replace(strDescription, "(\n|\r)", String.Empty, RegexOptions.Multiline);
+            strDescription = Regex.Replace(strDescription, "< *br */*>", "\n", RegexOptions.IgnoreCase & RegexOptions.Multiline);
+            strDescription = Regex.Replace(strDescription, "< */ *li */*>", "\n", RegexOptions.IgnoreCase & RegexOptions.Multiline);
+            strDescription = Regex.Replace(strDescription, "  +", " ", RegexOptions.Multiline);
+            strDescription = Regex.Replace(strDescription, "<[^>]*>", String.Empty, RegexOptions.Multiline);
+            strDescription = strDescription.Replace("&quot;", "\"").Replace("&amp;", "&").Replace("&apos;", "'").Replace("&lt;", "<").Replace("&gt;", ">");
+          }
+        }
+
+        #endregion
+
+        MP.ListItem(_List, strTitle, strSize, strURL, int.Parse(xmlDoc.SelectSingleNode("sites/site[@id='" + SiteName + "']/item").Attributes["type"].InnerText), strDescription, DateTime.ParseExact(_Node["pubDate"].InnerText.Replace("GMT", "+0000"), "ddd, dd MMM yyyy HH:mm:ss zzz", CultureInfo.InvariantCulture), (long)dblSize);
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+
+    #endregion
+
+  }
+}
---
+++ New Plugin/mpNZB.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+using MediaPortal.GUI.Library;
+
+namespace mpNZB
+{
+  public class mpNZB : GUIWindow, ISetupForm
+  {
+
+    #region SkinControlAttribute
+
+    [SkinControlAttribute(1)]
+    protected GUIButtonControl buttonRefresh = null;
+    [SkinControlAttribute(2)]
+    protected GUIButtonControl buttonFeeds = null;
+    [SkinControlAttribute(3)]
+    protected GUIButtonControl buttonGroups = null;
+    [SkinControlAttribute(4)]
+    protected GUIButtonControl buttonSearch = null;
+    [SkinControlAttribute(5)]
+    protected GUIButtonControl buttonClientStatus = null;
+
+    [SkinControlAttribute(50)]
+    protected GUIListControl listResults = null;
+
+    #endregion
+
+    #region Declarations
+
+    Sites Site;
+    Functions MP = new Functions();
+
+    #endregion
+
+    #region ISetupForm Members
+
+    // Returns the name of the plugin which is shown in the plugin menu
+    public string PluginName()
+    {
+      return "mpNZB";
+    }
+
+    // Returns the description of the plugin is shown in the plugin menu
+    public string Description()
+    {
+      return "NZB Downloader";
+    }
+
+    // Returns the author of the plugin which is shown in the plugin menu
+    public string Author()
+    {
+      return "Dustin Brett";
+    }
+
+    // show the setup dialog
+    public void ShowPlugin()
+    {
+      //frmSetup frmSetupDialog = new frmSetup();
+      //frmSetupDialog.Show();
+    }
+
+    // Indicates whether plugin can be enabled/disabled
+    public bool CanEnable()
+    {
+      return true;
+    }
+
+    // Get Windows-ID
+    public int GetWindowId()
+    {
+      // WindowID of windowplugin belonging to this setup
+      // enter your own unique code
+      return 8357;
+    }
+
+    // Indicates if plugin is enabled by default;
+    public bool DefaultEnabled()
+    {
+      return true;
+    }
+
+    // indicates if a plugin has it's own setup screen
+    public bool HasSetup()
+    {
+      return true;
+    }
+
+    /// <summary>
+    /// If the plugin should have it's own button on the main menu of MediaPortal then it
+    /// should return true to this method, otherwise if it should not be on home
+    /// it should return false
+    /// </summary>
+    /// <param name="strButtonText">text the button should have</param>
+    /// <param name="strButtonImage">image for the button, or empty for default</param>
+    /// <param name="strButtonImageFocus">image for the button, or empty for default</param>
+    /// <param name="strPictureImage">subpicture for the button or empty for none</param>
+    /// <returns>true : plugin needs it's own button on home
+    /// false : plugin does not need it's own button on home</returns>
+
+    public bool GetHome(out string strButtonText, out string strButtonImage,
+      out string strButtonImageFocus, out string strPictureImage)
+    {
+      strButtonText = MP.GetValueAsString("#Plugin", "DisplayName", String.Empty);
+      strButtonImage = String.Empty;
+      strButtonImageFocus = String.Empty;
+      strPictureImage = String.Empty;
+      return true;
+    }
+
+    // With GetID it will be an window-plugin / otherwise a process-plugin
+    // Enter the id number here again
+    public override int GetID
+    {
+      get
+      {
+        return 8357;
+      }
+
+      set
+      {
+      }
+    }
+
+    #endregion
+
+    #region Initialization
+
+    public override bool Init()
+    {
+      return Load(GUIGraphicsContext.Skin + @"\mpNZB.xml");
+    }
+
+    #endregion
+
+    #region Actions
+
+    protected override void OnPageLoad()
+    {
+      GUIPropertyManager.SetProperty("#Title", MP.GetValueAsString("#Plugin", "DisplayName", String.Empty));
+      GUIPropertyManager.SetProperty("#Information", String.Empty);
+      GUIPropertyManager.SetProperty("#Status", String.Empty);
+    }
+
+    protected override void OnPageDestroy(int newWindowId)
+    {
+    }
+
+    protected override void OnClicked(int controlId, GUIControl control, Action.ActionType actionType)
+    {
+      if (control == buttonRefresh) ;
+      if (control == buttonFeeds) SelectSite("Feeds");
+      if (control == buttonGroups) SelectSite("Groups");
+      if (control == buttonSearch) SelectSite("Search");
+      if (control == buttonClientStatus) ;
+      if (control == listResults) GUIPropertyManager.SetProperty("#Information", listResults.ListItems[listResults.SelectedListItemIndex].DVDLabel);
+
+      base.OnClicked(controlId, control, actionType);
+    }
+
+    protected override void OnShowContextMenu()
+    {
+    }
+
+    #endregion
+
+    #region Functions
+
+    private void ReadRSS(string _URI, GUIListControl _List)
+    {
+      try
+      {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(new MemoryStream(ASCIIEncoding.Default.GetBytes(MP.GetURI(_URI))));
+
+        _List.ListItems.Clear();
+
+        foreach (XmlNode xmlNode in xmlDoc.SelectNodes("rss/channel/item"))
+        {
+          Site.AddItem(xmlNode, _List);
+        }
+
+        GUIPropertyManager.SetProperty("#Status", "Found " + _List.Count.ToString() + " Items");
+      }
+      catch (Exception ex) { MP.Error(ex); }
+      finally
+      {
+        if (_List.Count > 0)
+        {
+          this.LooseFocus();
+          _List.Focus = true;
+        }
+      }
+    }
+
+    private void SelectSite(string _Type)
+    {
+      try
+      {
+        Site = new Sites(_Type);
+
+        if (Site.SiteName.Length > 0)
+        {
+          switch (_Type)
+          {
+            case "Feeds":
+              Site.SetFeed();
+              break;
+            case "Groups":
+              Site.SetGroup();
+              break;
+            case "Search":
+              Site.SetSearch();
+              break;
+          }
+
+          if (Site.FeedURI.Length > 0)
+          {
+            GUIPropertyManager.SetProperty("#Status", "Processing...");
+            ReadRSS(Site.FeedURI, listResults);
+            buttonRefresh.Disabled = false;
+          }
+        }
+      }
+      catch (Exception ex) { MP.Error(ex); }
+    }
+
+    #endregion
+
+  }
+}
